56911d4827a850206116212f15af3b50d2e86012
DaifMX <applediegoaif@gmail.com>
2025-05-27 08:32:38 +0000

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: 3030,
     allowedHosts: ['daif-201.ddns.me']
   },
